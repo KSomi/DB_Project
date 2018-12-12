1861ec5641bc3515d605ef76df8bed97beeeb1fc
Giauzar <45813044+Giauzar@users.noreply.github.com>
2018-12-12 17:11:52 +0900

    새로운 데이터베이스 선언문
    비밀번호 확장, 팀메이트 추가

---
+++ DB_Project.sql
@@ -0,0 +1,234 @@
+﻿--
+-- Create schema DB_Project
+--
+
+CREATE DATABASE IF NOT EXISTS DB_Project;
+USE DB_Project;
+
+
+--
+-- Definition of table `ADMIN`
+--
+
+DROP TABLE IF EXISTS `ADMIN`;
+CREATE TABLE `ADMIN` (
+  `ID` varchar(30) NOT NULL,
+  `Password` varchar(30) NOT NULL,
+  PRIMARY KEY (`ID`)
+) DEFAULT CHARSET=utf8;
+
+
+
+--
+-- Definition of table `CLIENT`
+--
+
+DROP TABLE IF EXISTS `CLIENT`;
+CREATE TABLE `CLIENT` (
+  `ID` varchar(30) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Phone` int(12),
+  `Rating` float(5),
+  `C_request_list` text,
+  PRIMARY KEY (`ID`)
+) DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `CLIENT`
+--
+
+/*!40000 ALTER TABLE `CLIENT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `CLIENT` ENABLE KEYS */;
+
+--
+-- Definition of table `FREELANCER`
+--
+
+DROP TABLE IF EXISTS `FREELANCER`;
+CREATE TABLE `FREELANCER` (
+  `ID` varchar(30) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Phone` int(12),
+  `Rating` float(5),
+  `Age` int(3),
+  `Major` text,
+  `Career` int(3),
+  `Proficiency` text,
+  `F_request_list` text,
+  PRIMARY KEY (`ID`)
+) DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `FREELANCER`
+--
+
+/*!40000 ALTER TABLE `FREELANCER` DISABLE KEYS */;
+/*!40000 ALTER TABLE `FREELANCER` ENABLE KEYS */;
+
+
+
+--
+-- Definition of table `TEAM`
+--
+
+DROP TABLE IF EXISTS `TEAM`;
+CREATE TABLE `TEAM` (
+  `Name` varchar(30) NOT NULL,
+  `Leader` varchar(30) NOT NULL,
+  PRIMARY KEY (`Name`),
+
+  CONSTRAINT t_leader
+  FOREIGN KEY (`Leader`) REFERENCES `FREELANCER`(`ID`)
+    ON DELETE CASCADE
+
+) DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `TEAMMATE`;
+CREATE TABLE `TEAMMATE` (
+  `Teamname` varchar(30) NOT NULL,
+  `Teammate` varchar(50) NOT NULL,
+
+  CONSTRAINT teamname
+  FOREIGN KEY (`Teamname`) REFERENCES `TEAM`(`Name`)
+    ON DELETE CASCADE  ON UPDATE CASCADE
+
+) DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `TEAM`
+--
+
+/*!40000 ALTER TABLE `TEAM` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TEAM` ENABLE KEYS */;
+
+
+--
+-- Definition of table `REQUEST`
+--
+
+DROP TABLE IF EXISTS `REQUEST`;
+CREATE TABLE `REQUEST` (
+  `NUM` int(30) NOT NULL,
+  `Upload_time` timestamp NOT NULL,
+  `C_id` varchar(30) NOT NULL,
+  `Budget` int(10),
+  `Start_date`timestamp,
+  `End_data`timestamp,
+  `Proficiency` text,
+  `Minimum_career` text,
+  `Min_num_person` int(5),
+  `Max_num_person` int(5),
+  `Accepted` tinyint(1) NOT NULL,
+  `Accept_f` varchar(30),
+  `Accept_t` varchar(30),
+  PRIMARY KEY (`NUM`),
+
+  CONSTRAINT client_id
+  FOREIGN KEY (`C_id`) REFERENCES `CLIENT`(`ID`)
+    ON DELETE CASCADE  ON UPDATE CASCADE,
+  CONSTRAINT accept_f
+  FOREIGN KEY (`Accept_f`) REFERENCES `FREELANCER`(`ID`)
+    ON DELETE CASCADE  ON UPDATE CASCADE,
+  CONSTRAINT team_name
+  FOREIGN KEY (`Accept_t`) REFERENCES `TEAM`(`Name`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+
+
+) DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `REQUEST`
+--
+
+/*!40000 ALTER TABLE `REQUEST` DISABLE KEYS */;
+/*!40000 ALTER TABLE `REQUEST` ENABLE KEYS */;
+
+
+
+--
+-- Definition of table `PORTFOLIO`
+--
+
+DROP TABLE IF EXISTS `PORTFOLIO`;
+CREATE TABLE `PORTFOLIO` (
+  `P_fid` varchar(30) NOT NULL,
+  `In` tinyint(1) NOT NULL,
+  `Out` tinyint(1) NOT NULL,
+  `P_Num` int(10) NOT NULL,
+  PRIMARY KEY (`P_Num`),
+
+  CONSTRAINT pfid
+  FOREIGN KEY (`P_fid`) REFERENCES `FREELANCER`(`ID`)
+    ON DELETE CASCADE  ON UPDATE CASCADE
+
+) DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `PORTFOLIO`
+--
+
+/*!40000 ALTER TABLE `PORTFOLIO` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PORTFOLIO` ENABLE KEYS */;
+
+--
+-- Definition of table `MESSAGE`
+--
+
+DROP TABLE IF EXISTS `MESSAGE`;
+CREATE TABLE `MESSAGE` (
+  `Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `C_id` varchar(30) NOT NULL,
+  `F_id` varchar(30) NOT NULL,
+  `Text` text,
+  `Mnum` int(30) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`Mnum`),
+
+  CONSTRAINT mcid
+  FOREIGN KEY (`C_id`) REFERENCES `CLIENT`(`ID`)
+    ON DELETE CASCADE  ON UPDATE CASCADE,
+  CONSTRAINT mfid
+  FOREIGN KEY (`F_id`) REFERENCES `FREELANCER`(`ID`)
+    ON DELETE CASCADE  ON UPDATE CASCADE
+
+) DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `MESSAGE`
+--
+
+/*!40000 ALTER TABLE `MESSAGE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `MESSAGE` ENABLE KEYS */;
+
+
+
+--
+-- Definition of table `PROFICIENCY`
+--
+
+DROP TABLE IF EXISTS `PROFICIENCY`;
+CREATE TABLE `PROFICIENCY` (
+  `ID` varchar(30) NOT NULL,
+  `Language` varchar(30) NOT NULL,
+  `Score` int(5) NOT NULL,
+  PRIMARY KEY (`ID`),
+
+  CONSTRAINT profid
+  FOREIGN KEY (`ID`) REFERENCES `FREELANCER`(`ID`)
+    ON DELETE CASCADE  ON UPDATE CASCADE
+
+) DEFAULT CHARSET=utf8;
+
+
+--
+-- Dumping data for table `PROFICIENCY`
+--
+
+/*!40000 ALTER TABLE `PROFICIENCY` DISABLE KEYS */;
+/*!40000 ALTER TABLE `PROFICIENCY` ENABLE KEYS */;
