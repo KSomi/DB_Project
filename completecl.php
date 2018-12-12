269b9516b2344cf729b13898c9c8fd58d06cfb45
Giauzar <45813044+Giauzar@users.noreply.github.com>
2018-12-13 03:20:09 +0900

    Add files via upload

--- completecl.php
+++ completecl.php
@@ -2,8 +2,19 @@
 include "dbConnect.php";
 
 $req_num = $_POST['req_num'];
+$star = $_POST['star'];
 
 $sql = "UPDATE request SET Complete = 2 WHERE '{$req_num}' = NUM";
+$dbConnect->query($sql);
+
+$sql = "SELECT * FROM request WHERE '{$req_num}' = NUM";
+$res = $dbConnect->query($sql);
+
+$row = mysqli_fetch_array($res);
+
+$id = $row['Accepted'];
+
+$sql = "UPDATE freelancer SET Rating = '{$star}' WHERE ID = '{$id}'";
 $dbConnect->query($sql);
 
   echo "<script>alert(\"승인 완료\");</script>";
