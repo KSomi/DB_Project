44f8f259ccd00df2e0afb3d5e933933a1811a654
Giauzar <45813044+Giauzar@users.noreply.github.com>
2018-12-13 03:56:04 +0900

    Add files via upload

--- completecl.php
+++ completecl.php
@@ -14,8 +14,25 @@ $row = mysqli_fetch_array($res);
 
 $id = $row['Accepted'];
 
-$sql = "UPDATE freelancer SET Rating = '{$star}' WHERE ID = '{$id}'";
+$sql = "INSERT INTO portfolio(P_fid, num) VALUES('{$id}','{$req_num}')";
+$row = $dbConnect->query($sql);
+
+$sql = "SELECT * FROM freelancer WHERE '{$id}' = ID";
+$res = $dbConnect->query($sql);
+$row = mysqli_fetch_array($res);
+
+$exp = $row['exp'];
+$exp1 = $exp + 1;
+$rat = $row['Rating'];
+$rating = (($rat * $exp)+$star)/($exp1);
+
+
+$sql = "UPDATE freelancer SET Rating = '{$rating}' WHERE ID = '{$id}'";
 $dbConnect->query($sql);
+
+$sql = "UPDATE freelancer SET exp = '{$exp1}' WHERE ID = '{$id}'";
+$dbConnect->query($sql);
+
 
   echo "<script>alert(\"승인 완료\");</script>";
   echo("<script>location.href='ReQuestINGCL.html';</script>");
